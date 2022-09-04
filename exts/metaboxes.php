5830b602eaa06ba694b87ff40ede549c80a45a0d
grimroy <grim.roy@gmail.com>
2022-09-04 18:22:57 +0600

    New Routes: Primary menu ACF Icon

--- exts/metaboxes.php
+++ exts/metaboxes.php
@@ -12,20 +12,26 @@ class MetaBoxes
 {
     public function __construct()
     {
-        add_filter('add_meta_boxes', [$this, 'add_image_to_menuItem']);
+        add_filter('wp_setup_nav_menu_item', [$this, 'add_image_to_menuItem']);
+        add_action('wp_update_nav_menu_item', [$this, 'save_image_to_menuItem'], 10, 3);
+        add_filter('wp_edit_nav_menu_walker', [$this, 'walker_image_to_menuItem'], 10, 2);
     }
-    public function add_image_to_menuItem()
+    public function add_image_to_menuItem($menus)
     {
-        add_meta_box(
-            'menu_image',                 // Unique ID
-            'Icon/Image',      // Box title
-            [$this, 'menu_image_html'],  // Content callback, must be of type callable
-            'nav_menu_item'                           // Post type
-        );
+        $menus->navIcon = get_post_meta($menus->ID, '_menu_item_navIcon', true);
+        return $menus;
     }
-    public function menu_image_html($post)
+    public function save_image_to_menuItem($id, $db_id, $args)
     {
-?><label for="wporg_field">Description for this field</label><?php
-                                                                    }
-                                                                }
-//new MetaBoxes(); TODO:need to develop
+        if (is_array($_REQUEST['menu-item-navIcon'])) {
+            $navIcon = $_REQUEST['menu-item-navIcon'][$db_id];
+            update_post_meta($db_id, '_menu_item_navIcon', $navIcon);
+        }
+    }
+    public function walker_image_to_menuItem($walker, $menu_id)
+    {
+        return 'Walker_Nav_Menu_Edit_Custom';
+    }
+}
+//new MetaBoxes();
+                                                                //TODO:need to develop
